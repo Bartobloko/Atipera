75daaf5199e9a4d97bf963d939efe7cb22b39538
Bartobloko <bartobloko@gmail.com>
2025-06-24 12:40:06 +0200

    add actions column with edit button to periodic table

--- src/features/periodic-table/periodic-table.ts
+++ src/features/periodic-table/periodic-table.ts
@@ -2,9 +2,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { PeriodicElementsStore } from '../../utils/store/periodic-elements.store';
 import { PeriodicElement } from '../../utils/models/periodic-element';
+import {EditElementDialog} from '../edit-element-dialog/edit-element-dialog';
 
 @Component({
   selector: 'app-periodic-table',
@@ -13,21 +17,22 @@ import { PeriodicElement } from '../../utils/models/periodic-element';
     CommonModule,
     MatTableModule,
     MatProgressSpinnerModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './periodic-table.html',
   styleUrl: './periodic-table.scss'
 })
 export class PeriodicTable implements OnInit {
   private readonly store = inject(PeriodicElementsStore);
+  private readonly dialog = inject(MatDialog);
 
-  // Sygnały z store
   readonly elements = this.store.elements;
   readonly loading = this.store.loading;
   readonly elementsCount = this.store.elementsCount;
 
-  // Kolumny zgodnie z najnowszą dokumentacją Material
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
 
   ngOnInit(): void {
     this.store.loadElements();
@@ -36,5 +41,21 @@ export class PeriodicTable implements OnInit {
   onRowClick(element: PeriodicElement): void {
     this.store.selectElement(element);
     console.log('Selected element:', element);
+  }
+
+  onEditElement(element: PeriodicElement, event: Event): void {
+    event.stopPropagation();
+
+    const dialogRef = this.dialog.open(EditElementDialog, {
+      width: '500px',
+      data: { element }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.store.updateElement(result);
+        console.log('Element updated:', result);
+      }
+    });
   }
 }
