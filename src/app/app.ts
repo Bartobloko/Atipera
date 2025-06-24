a7df1841b66a9e9c84147a2f2abe6c7afef92d58
Bartobloko <bartobloko@gmail.com>
2025-06-24 12:33:10 +0200

    implement Angular Material table and loading spinner to periodic-table

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {Home} from '../core/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/features/periodic-table/periodic-table.ts
+++ src/features/periodic-table/periodic-table.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { PeriodicElementsStore } from '../../utils/store/periodic-elements.store';
+import { PeriodicElement } from '../../utils/models/periodic-element';
 
 @Component({
   selector: 'app-periodic-table',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatCardModule
+  ],
   templateUrl: './periodic-table.html',
   styleUrl: './periodic-table.scss'
 })
-export class PeriodicTable {
+export class PeriodicTable implements OnInit {
+  private readonly store = inject(PeriodicElementsStore);
 
+  // Sygnały z store
+  readonly elements = this.store.elements;
+  readonly loading = this.store.loading;
+  readonly elementsCount = this.store.elementsCount;
+
+  // Kolumny zgodnie z najnowszą dokumentacją Material
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+  ngOnInit(): void {
+    this.store.loadElements();
+  }
+
+  onRowClick(element: PeriodicElement): void {
+    this.store.selectElement(element);
+    console.log('Selected element:', element);
+  }
 }
